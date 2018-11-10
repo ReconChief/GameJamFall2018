b2a9584833ae81da735f421e462e0ebaa74f6a13
ReconChief <32560933+ReconChief@users.noreply.github.com>
2018-11-10 15:01:10 -0500

    Added "Rocks" and a Couple of Scripts

---
+++ GameJamFall2019/Assets/Scripts/GameController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject returnPoint;
+    public GameObject player;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void ReturnPlayer()
+    {
+        player.gameObject.transform.position = returnPoint.transform.position;
+    }
+}
---
+++ GameJamFall2019/Assets/Scripts/OutofBoundaries.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutofBoundaries : MonoBehaviour
+{
+    private GameController gc;
+
+    void Start()
+    {
+        gc = GameObject.FindWithTag("GameController").GetComponent<GameController>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            gc.ReturnPlayer();
+        }
+    }
+}
