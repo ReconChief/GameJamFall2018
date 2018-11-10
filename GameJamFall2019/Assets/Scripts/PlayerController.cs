f6e24da6550f9430f4b33d1a030e8fec79b8787b
Carlos <carlos@killersnails.com>
2018-11-10 17:39:19 -0500

    Epic player updates with animations <33333!!

--- GameJamFall2019/Assets/Scripts/PlayerController.cs
+++ GameJamFall2019/Assets/Scripts/PlayerController.cs
@@ -1,11 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using PixelEngine;
 
 namespace GameJam2018 {
 	public class PlayerController : MonoBehaviour {
+		private static readonly AnimationParameter IsMovingId = "Is Moving";
+		private static readonly AnimationParameter FinalMovementSpeedId = "Final Movement Speed";
+
 		[SerializeField] private PlayerMoveSettings moveSettings;
 		[SerializeField] private PlayerCamera playerCameraPrefab;
+
+		//[Header(PixelEngineConstants.AnimatedOnlyProperties)]
+		//[Range(0, 1)]
+		//[SerializeField] private float movementFactor = 1
 
 		private PlayerCamera camera;
 		private new Rigidbody rigidbody;
@@ -14,6 +22,7 @@ namespace GameJam2018 {
 		private float horizontal;
 		private float vertical;
 		private Vector3 desiredVelocity;
+		private float finalMovementSpeed;
 
 		public void Awake() {
 			if (moveSettings == null)
@@ -30,9 +39,9 @@ namespace GameJam2018 {
 			Vector3 currentVelocity = rigidbody.velocity;
 
 			desiredVelocity.Set(horizontal, 0, vertical);
-			desiredVelocity *= moveSettings.JogSpeed;
+
+			desiredVelocity *= finalMovementSpeed;
 			desiredVelocity = camera.XZOrientation.TransformDirection(desiredVelocity);
-			transform.forward = desiredVelocity;
 
 			rigidbody.AddForce(desiredVelocity - currentVelocity, ForceMode.VelocityChange);
 		}
@@ -41,7 +50,22 @@ namespace GameJam2018 {
 			horizontal = Input.GetAxis("Horizontal");
 			vertical = Input.GetAxis("Vertical");
 
+			//Debating whether or not to do this with physics too... hm...
+			if (desiredVelocity.sqrMagnitude > 0.01f)
+				transform.forward = Vector3.Lerp(transform.forward, desiredVelocity.normalized, moveSettings.TurnLerpSpeed * Time.deltaTime);
 
+			UpdateFinalMovementSpeed();
+			UpdateContinuousAnimatorParameters();
+		}
+
+		private void UpdateFinalMovementSpeed() {
+			finalMovementSpeed = moveSettings.JogSpeed * Mathf.Max(Mathf.Abs(horizontal), Mathf.Abs(vertical));
+		}
+
+		private void UpdateContinuousAnimatorParameters() {
+			animator.SetFloat(FinalMovementSpeedId, finalMovementSpeed);
+			animator.SetBool(IsMovingId, finalMovementSpeed > 0);
+				//&& Time.time - timeLastAttacked > AttackUnjoggableTime); //For later when we add attacks
 		}
 	}
 }
--- GameJamFall2019/Assets/Scripts/PlayerMoveSettings.cs
+++ GameJamFall2019/Assets/Scripts/PlayerMoveSettings.cs
@@ -4,9 +4,14 @@ namespace GameJam2018 {
 	[CreateAssetMenu(fileName = "New Player Move Settings", menuName = GameJamConstants.CreateAssetMenu + "Player Move Settings")]
 	public class PlayerMoveSettings : ScriptableObject {
 		[SerializeField] private float jogSpeed = 5;
+		[SerializeField] private float turnLerpSpeed = 10;
 
 		public float JogSpeed {
 			get { return jogSpeed; }
+		}
+
+		public float TurnLerpSpeed {
+			get { return turnLerpSpeed; }
 		}
 	}
 }
